7c4015ac0e59048e769f8604e1600abdd5ba732f
Elle Whaley <elliewhaley11@gmail.com>
2019-05-28 16:51:45 +0100

    added a basic user model

--- models/User.py
+++ models/User.py
@@ -1,11 +1,24 @@
-#
-# class User(db.Entity):
-#     username = Required(str, unique=True)
-#     email = Required(str, unique=True)
-#     password_hash = Required(str)
-#     postcode = Required(str)
-#     closest_station = Required(str)
-#
+# from datetime import datetime, timedelta
+# import bcrypt
+# import jwt
+from pony.orm import Required
+from marchmallow import Schema, fields
+from app import db
+
+class User(db.Entity):
+    username = Required(str, unique=True)
+    email = Required(str, unique=True)
+    password_hash = Required(str)
+    postcode = Required(str)
+    closest_station = Required(str)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    email = fields.Str(required=True)
+    postcode = fields.Str(required=True)
+    closest_station = fields.Str(required=True)
+
 #     def is_pass_valid(self, plaintext):
 #         return bcrypt.checkpw(plaintext.encode('utf8')
 #         self.password_hash.encode('utf8'))
